f19bf41ea094d056179af5ddc3f7a1c42066fb78
deps7 <bdeps18@gmail.com>
2026-02-05 11:41:41 +0100

    JS zadaci

--- 2_JS/vjezba04/zad/app.js
+++ 2_JS/vjezba04/zad/app.js
@@ -88,10 +88,127 @@ document.getElementById('izvedi').addEventListener('click', () => {
 
   }
 
+  //---------------------------------------------------- ZADAĆA -----------------------------------------------------------------------
+
+  //ZADATAK 3
+
+  if (zadatak === '3') {
+
+    const brA = Number(a)
+    const brB = Number(b)
+    const brC = Number(c)
+    const brD = Number(d)
+
+    const zbroj = brA + brB + brC + brD;
+
+    rezultat.innerHTML = `${zbroj}`;
+
+    return;
+
+  }
+
+
+  // ZADATAK 4
+
+  if (zadatak === '4') {
+
+    const godinaRodenja = Number(a);
+    const trenutnaGodina = new Date().getFullYear();
+    const dob = trenutnaGodina - godinaRodenja;
+
+    if (dob >= 18) {
+      rezultat.innerHTML = `Punoljetan/na si.`;
+    } else {
+      rezultat.innerHTML = `Nisi punoljetan/na.`;
+    }
+
+    return;
+  }
+
+  //ZADATAK 5
+
+  if (zadatak === '5') {
+
+    const broj = Number(a);
+
+    if (broj > 0) {
+      rezultat.innerHTML = 'Pozitivan.';
+    } else if (broj < 0) {
+      rezultat.innerHTML = 'Negativan.';
+    } else {
+      rezultat.innerHTML = 'Nula.';
+    }
+
+    return;
+
+  }
+
+  //ZADATAK 6
+
+  if (zadatak === '6') {
+
+    const broj = Number(a);
+
+    if (broj % 2 === 0) {
+      rezultat.innerHTML = 'Paran';
+    } else {
+      rezultat.innerHTML = 'Neparan';
+    }
+
+    return;
+
+  }
+
+
+  //ZADATAK 7
+
+  if (zadatak === '7') {
+
+    const bodovi = Number(a);
+
+    if (bodovi >= 90 && bodovi <= 100) {
+      rezultat.innerHTML = 'Odličan (5)';
+    } else if (bodovi >= 80 && bodovi < 90) {
+      rezultat.innerHTML = 'Vrlo dobar (4)';
+    } else if (bodovi >= 70 && bodovi < 80) {
+      rezultat.innerHTML = 'Dobar (3)';
+    } else if (bodovi >= 60 && bodovi < 70) {
+      rezultat.innerHTML = 'Dovoljan (2)';
+    } else {
+      rezultat.innerHTML = 'Nedovoljan (1)';
+    }
+    return;
+
+  }
+
+
+  //ZADATAK 8
+
+  if (zadatak === '8') {
+
+    const brA = Number(a);
+    const brB = Number(b);
+
+    if (brA % brB === 0) {
+      rezultat.innerHTML = 'Djeljiv je';
+    } else {
+      rezultat.innerHTML = 'Nije djeljiv';
+    }
+    return;
+
+  }
+
+
+
+
+
+
+
+
+
+
+
   
   // ovo će se ispisati ako u HTML dodatke option za zadatak a ovdje ga ne obradite
-  rezultat.innerHTML = `Nepoznati zadatak ${zadatak}`;
+rezultat.innerHTML = `Nepoznati zadatak ${zadatak}`;
 });
-
-
-
