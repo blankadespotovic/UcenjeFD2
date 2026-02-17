2ffd1283cfe91dc72d2334ae293173a9e31b28e7
deps7 <bdeps18@gmail.com>
2026-02-17 21:17:41 +0100

    while petlja

--- 2_JS/vjezba07/zad/app.js
+++ 2_JS/vjezba07/zad/app.js
@@ -47,9 +47,9 @@ document.getElementById('izvedi').addEventListener('click', () => {
 
       for (let i = 2; i < a1; i++) {
         //if(i%10000 === 0){
-          console.log(a1, '%', i, '=', a1 % i);
+        console.log(a1, '%', i, '=', a1 % i);
         //}
-          if (a1 % i === 0) {
+        if (a1 % i === 0) {
           prim = false;
           break;
         }
@@ -66,13 +66,13 @@ document.getElementById('izvedi').addEventListener('click', () => {
       //Za uneseni cijeli broj u polje B veći od 2 i manji od 1000 ispiši da li je savršen broj ili ne.
 
       const b2 = Number(b);
-      
-      if(!b2){
+
+      if (!b2) {
         rezultat.innerHTML = 'Nisi unio broj!';
         return;
       }
-      
-      if(b2 <2 || b2 > 1000){
+
+      if (b2 < 2 || b2 > 1000) {
         rezultat.innerHTML('Broj mora biti veći od 2 i manji od 1000')
         return;
       }
@@ -96,48 +96,75 @@ document.getElementById('izvedi').addEventListener('click', () => {
 
       let sum = 0;
 
-      for(let i=1; i < b2; i++){
-        if(b2 % i === 0){
+      for (let i = 1; i < b2; i++) {
+        if (b2 % i === 0) {
           sum += i;
         }
       }
 
-      if (sum === b2){
+      if (sum === b2) {
         rezultat.innerHTML = `${b2} je savršen broj.`;
-      }else{
+      } else {
         rezultat.innerHTML = `${b2} nije savršen broj.`;
       }
 
 
       // kraj rješavanje 2. zadatak
-    break;
+      break;
     case '3':
       // rješavanje 3. zadatak
       //Za uneseni cijeli broj u polje B veći od 2 i manji od 100 ispiši da li je Armstrongov broj ili ne.
 
       // kraj rješavanje 3. zadatak
-    break;
+      break;
     case '4':
       // rješavanje 4. zadatak
       //Za unesenu riječ u polje A provjerite da li je palindrom ili ne
+      //Ružan Edo ode na žur
+      //ružanedoodenažur
+
+      let s = '';
+
+      for (let i = 0; i < a.length; i++) {
+        if (a[i] != ' ') {
+          s += a[i].toLowerCase()
+        }
+      }
+      console.log(s);
+
+      let duljina = s.length;
+      let palindrom = true;
+
+      for (let i = 0; i < duljina / 2; i++) {
+
+        if (s[i] !== s[duljina - 1 - i]) {
+          palindrom = false;
+        }
+      }
+
+      if (palindrom) {
+        rezultat.innerHTML = 'Palindrom'
+      } else {
+        rezultat.innerHTML = 'Nije palindrom'
+      }
 
       // kraj rješavanje 4. zadatak
-    break;
+      break;
     case '5':
       // rješavanje 5. zadatak
 
       // kraj rješavanje 5. zadatak
-    break;
+      break;
     case '6':
       // rješavanje 6. zadatak
 
       // kraj rješavanje 6. zadatak
-    break;
+      break;
     case '7':
       // rješavanje 7. zadatak
 
       // kraj rješavanje 7. zadatak
-    break;
+      break;
 
     default:
       rezultat.innerHTML = `Nepoznati zadatak ${zadatak}`;
@@ -145,60 +172,60 @@ document.getElementById('izvedi').addEventListener('click', () => {
 
 });
 const poveznica = document.getElementById('poveznica');
-poveznica.href='https://enciklopedija.hr/clanak/primbrojevi';
+poveznica.href = 'https://enciklopedija.hr/clanak/primbrojevi';
 document.getElementById('zadatak').addEventListener('change', (event) => {
-   switch (event.target.value) {
+  switch (event.target.value) {
     case '1':
-      poveznica.href='https://enciklopedija.hr/clanak/primbrojevi';
-    break;
+      poveznica.href = 'https://enciklopedija.hr/clanak/primbrojevi';
+      break;
     case '2':
-      poveznica.href='https://www.enciklopedija.hr/clanak/savrseni-broj';
-    break;
+      poveznica.href = 'https://www.enciklopedija.hr/clanak/savrseni-broj';
+      break;
     case '3':
-      poveznica.href='https://hafura.wordpress.com/2017/09/13/armstrongov-broj-ispitni-zadatak/';
-    break;
+      poveznica.href = 'https://hafura.wordpress.com/2017/09/13/armstrongov-broj-ispitni-zadatak/';
+      break;
     case '4':
-      poveznica.href='https://www.enciklopedija.hr/clanak/palindrom';
-    break;
+      poveznica.href = 'https://www.enciklopedija.hr/clanak/palindrom';
+      break;
     case '5':
-      poveznica.href='https://www.coursera.org/articles/what-are-stop-words';
-    break;
+      poveznica.href = 'https://www.coursera.org/articles/what-are-stop-words';
+      break;
     case '6':
-      poveznica.href='https://enciklopedija.hr/clanak/samoglasnik';
-    break;
+      poveznica.href = 'https://enciklopedija.hr/clanak/samoglasnik';
+      break;
     case '7':
-      poveznica.href='https://hjp.znanje.hr/index.php?show=search_by_id&id=d1ZlWhU%3D';
-    break;
+      poveznica.href = 'https://hjp.znanje.hr/index.php?show=search_by_id&id=d1ZlWhU%3D';
+      break;
     default:
-      poveznica.href='#';
+      poveznica.href = '#';
   }
 });
 
 
 
 const stopRijeci = [
-  'a', 'ako', 'ali', 'bi', 'bih', 'bila', 'bili', 'bilo', 'bio', 'bismo', 
-  'biste', 'biti', 'bumo', 'da', 'do', 'duž', 'ga', 'hoće', 'hoćemo', 'hoćete', 
-  'hoćeš', 'hoću', 'i', 'iako', 'idemo', 'ideš', 'ide', 'iz', 'iza', 'iznad', 
-  'ja', 'jako', 'jer', 'jesmo', 'jeste', 'jesu', 'je', 'kad', 'kada', 'kao', 
-  'kroz', 'li', 'me', 'mene', 'meni', 'mi', 'mimo', 'mene', 'moj', 'moja', 
-  'moje', 'mu', 'na', 'nad', 'nakon', 'nam', 'nama', 'nas', 'naš', 'naša', 
-  'naše', 'našu', 'ne', 'nego', 'neka', 'neki', 'nekog', 'neko', 'nema', 'netko', 
-  'neće', 'nećemo', 'nećete', 'nećeš', 'neću', 'ni', 'nije', 'nijedan', 'nikad', 
-  'nismo', 'niste', 'nisu', 'njega', 'njegov', 'njegova', 'njegovo', 'njemu', 'njen', 
-  'njena', 'njeno', 'njih', 'njihov', 'njihova', 'njihovo', 'njim', 'njima', 'njoj', 
-  'nju', 'o', 'od', 'odmah', 'on', 'ona', 'onaj', 'onam', 'onamo', 'one', 
-  'oni', 'ono', 'onolika', 'onoliko', 'onoliki', 'onoliku', 'ova', 'ovaj', 'ovdje', 
-  'ove', 'ovi', 'ovo', 'ovuda', 'pa', 'pak', 'po', 'pod', 'pored', 'poslije', 
-  'povrh', 'preko', 'pri', 'pred', 'prema', 'sa', 'sam', 'samo', 'se', 'sebe', 
-  'sebi', 'si', 'smo', 'ste', 'su', 'sve', 'svi', 'svog', 'svoj', 'svoja', 
-  'svoje', 'ta', 'taj', 'tako', 'tamo', 'te', 'tebe', 'tebi', 'ti', 'to', 
-  'točno', 'u', 'uz', 'vam', 'vama', 'vas', 'vaš', 'vaša', 'vaše', 'već', 
-  'vi', 'vrlo', 'za', 'zar', 'će', 'ćemo', 'ćete', 'ćeš', 'ću', 'što', 'the', 
-  'and', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 
-  'had', 'do', 'does', 'did', 'but', 'if', 'or', 'because', 'as', 'until', 
-  'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 
-  'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 
-  'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 
+  'a', 'ako', 'ali', 'bi', 'bih', 'bila', 'bili', 'bilo', 'bio', 'bismo',
+  'biste', 'biti', 'bumo', 'da', 'do', 'duž', 'ga', 'hoće', 'hoćemo', 'hoćete',
+  'hoćeš', 'hoću', 'i', 'iako', 'idemo', 'ideš', 'ide', 'iz', 'iza', 'iznad',
+  'ja', 'jako', 'jer', 'jesmo', 'jeste', 'jesu', 'je', 'kad', 'kada', 'kao',
+  'kroz', 'li', 'me', 'mene', 'meni', 'mi', 'mimo', 'mene', 'moj', 'moja',
+  'moje', 'mu', 'na', 'nad', 'nakon', 'nam', 'nama', 'nas', 'naš', 'naša',
+  'naše', 'našu', 'ne', 'nego', 'neka', 'neki', 'nekog', 'neko', 'nema', 'netko',
+  'neće', 'nećemo', 'nećete', 'nećeš', 'neću', 'ni', 'nije', 'nijedan', 'nikad',
+  'nismo', 'niste', 'nisu', 'njega', 'njegov', 'njegova', 'njegovo', 'njemu', 'njen',
+  'njena', 'njeno', 'njih', 'njihov', 'njihova', 'njihovo', 'njim', 'njima', 'njoj',
+  'nju', 'o', 'od', 'odmah', 'on', 'ona', 'onaj', 'onam', 'onamo', 'one',
+  'oni', 'ono', 'onolika', 'onoliko', 'onoliki', 'onoliku', 'ova', 'ovaj', 'ovdje',
+  'ove', 'ovi', 'ovo', 'ovuda', 'pa', 'pak', 'po', 'pod', 'pored', 'poslije',
+  'povrh', 'preko', 'pri', 'pred', 'prema', 'sa', 'sam', 'samo', 'se', 'sebe',
+  'sebi', 'si', 'smo', 'ste', 'su', 'sve', 'svi', 'svog', 'svoj', 'svoja',
+  'svoje', 'ta', 'taj', 'tako', 'tamo', 'te', 'tebe', 'tebi', 'ti', 'to',
+  'točno', 'u', 'uz', 'vam', 'vama', 'vas', 'vaš', 'vaša', 'vaše', 'već',
+  'vi', 'vrlo', 'za', 'zar', 'će', 'ćemo', 'ćete', 'ćeš', 'ću', 'što', 'the',
+  'and', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has',
+  'had', 'do', 'does', 'did', 'but', 'if', 'or', 'because', 'as', 'until',
+  'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between',
+  'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to',
+  'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again',
   'further', 'then', 'once'
 ];
