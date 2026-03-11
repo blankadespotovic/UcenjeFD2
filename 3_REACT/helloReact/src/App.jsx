9490478ff749811103c79fd8f555ceb4b973fb43
deps7 <bdeps18@gmail.com>
2026-03-11 20:19:40 +0100

    Rad s modulima i upoznavanje Reacta

--- 3_REACT/helloReact/src/App.jsx
+++ 3_REACT/helloReact/src/App.jsx
@@ -1,18 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Container from 'react-bootstrap/Container';
-import Accordion from 'react-bootstrap/Accordion';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+
 
 import slika from './assets/srce.jpg'
+import { Accordion, Button, Container, Form, Modal, Nav, Navbar, NavDropdown } from 'react-bootstrap'
 
 function App() {
 
   return (
     <Container>
+
+      <a href="moduli/index.html">Rad s modulima</a>
+
+      <hr />
+    
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
           <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
@@ -77,6 +77,13 @@ function App() {
           </Modal.Footer>
         </Modal.Dialog>
       </div>
+
+      <Form.Label htmlFor="inputPassword5">Password</Form.Label>
+      <Form.Control
+        type="password"
+        id="inputPassword5"
+        aria-describedby="passwordHelpBlock"
+      />
 
     </Container>
   )
