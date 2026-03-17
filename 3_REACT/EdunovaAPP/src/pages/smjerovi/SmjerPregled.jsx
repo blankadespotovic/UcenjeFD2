cb74e78db8f565d5ba9a97d2c06325b8683905f9
deps7 <bdeps18@gmail.com>
2026-03-17 20:11:13 +0100

    Dodavanje liste korisnika

--- 3_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
+++ 3_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react"
+import SmjerService from "../../services/smjerovi/SmjerService"
+
 export default function SmjerPregled(){
+
+    const [smjerovi, setSmjerovi] = useState([])
+
+    useEffect(()=>{
+        ucitajSmjerove()
+    },[])
+
+    async function ucitajSmjerove(){
+        await SmjerService.get().then((odgovor)=>{
+            setSmjerovi(odgovor.data)
+        })
+    }
+
+
     return(
         <>
-        Ovdje će doći pregled smjerova
+        <ol>
+            {smjerovi && smjerovi.map((smjer)=>(
+                <li>{smjer.naziv} </li>
+            ))}
+        </ol>
         </>
     )
 }
